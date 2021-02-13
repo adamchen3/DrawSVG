2f7d893d782f9b8110808bf448497e4e04956e1a
TheNumbat <mjslater@andrew.cmu.edu>
2021-02-13 12:24:32 -0500

    change runcmake to scotty3d one

--- runcmake_win.bat
+++ runcmake_win.bat
@@ -1,5 +1,9 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat" > NUL 2> NUL
 mkdir build
-cd build
+pushd build
 cmake ..
-cd ..
-pause
+if "%1"=="configure" exit
+if [%1]==[] (cmake --build . --config RelWithDebInfo)
+if not [%1]==[] (cmake --build . --config %1)
+popd
